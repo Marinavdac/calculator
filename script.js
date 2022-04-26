5960121ace9da063207f70f82b7ed2674a5d329b
Marina Andrade Catão <marinavdac@gmail.com>
2022-04-26 13:24:00 -0300

    feat: create class selected for keys

--- script.js
+++ script.js
@@ -1,11 +1,39 @@
 const display = document.getElementById('display');
+const resetAll = document.querySelector('.ce');
+const result = document.querySelector('.result');
+const plus = document.querySelector('.plus');
+const options = document.getElementById('options');
+const selected = document.querySelector('.selected');
 
+//saves operations
 display.addEventListener('input', () => {
-    const savedItems = localStorage.setItem('numbers', display.value);
-    return savedItems;
-})
+ const savedItems = localStorage.setItem('numbers', display.value);
+ return savedItems;
+});
 
-window.onload = function() {
-    const show = localStorage.getItem('numbers');
-    return display.value = show;
-  };
+window.onload = function () {
+ const show = localStorage.getItem('numbers');
+ return (display.value = show);
+};
+
+//uses btn CE to reset to clear display
+resetAll.addEventListener('click', () => {
+ display.value = '';
+});
+
+//uses the key ' = '
+function operate(number1, operation, number2) {
+ return operation(number1, number2);
+}
+
+result.addEventListener('click', operate);
+
+function selection(event) {
+    selected.classList.remove('selected');
+    console.log(selected);
+    event.target.classList.add('selected');
+
+ }
+ 
+
+options.addEventListener('click', selection);
